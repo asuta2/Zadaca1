b2af2bbcd5bfd2b2b46764603e85a138c11e558c
EkkoOwO <asuta2@etf.unsa.ba>
2022-11-19 17:40:01 +0100

    Ispravljena main metoda za unos preko konzole

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -5,18 +5,6 @@ public class App
     public static void main(String[] args)
     {
         ExpressionEvaluator temp = new ExpressionEvaluator();
-        double ispis= temp.evaluate(spoji(args));
-        System.out.println(ispis);
-
-    }
-    public static String spoji(String[] args)
-    {
-        String ispis="";
-        String sp="";
-        for(String x: args) {
-            ispis = ispis + sp + x;
-            sp = " ";
-        }
-        return ispis;
+        System.out.println(temp.evaluate(args[0]));
     }
 }
